1b7460e9d11cc5b3b20cb9893cd503d1f3a06b80
Nancia <rajerisonnancia@gmail.com>
2024-02-01 08:03:58 +0300

    Add illustration into Service model

--- models/service.js
+++ models/service.js
@@ -4,7 +4,8 @@ const serviceData = new mongoose.Schema({
     name: String,
     price: Number,
     duration: Number,
-    commission: Number
+    commission: Number,
+    illustration: String 
 });
 
 const Service = mongoose.model('Service', serviceData);
