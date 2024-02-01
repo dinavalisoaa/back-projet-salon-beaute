50e4c3a10b8f9d742a3dbb6fc49b0f2f0b9659f6
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-01 14:56:59 +0300

    fix max content uploaded

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -6,6 +6,6 @@ router.get('/service', serviceController.getAllService);
 router.post('/service', serviceController.createService);
 router.get('/service/:id', serviceController.getService);
 router.put('/service/:id', serviceController.updateService);
-router.delete('/servsice/:id', serviceController.deleteService);
+router.delete('/service/:id', serviceController.deleteService);
 
 module.exports = router;
