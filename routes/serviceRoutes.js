c2d0f20185c117bd94959148fda9049ebd5e30f4
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-05 00:12:14 +0300

    Update serviceRoutes.js

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -4,6 +4,7 @@ const serviceController = require('../controllers/serviceController');
 
 router.get('/services', serviceController.getAllService);
 router.post('/service', serviceController.createService);
+router.get('/service/sum', serviceController.sumService);
 router.get('/service/:id', serviceController.getService);
 router.put('/service/:id', serviceController.updateService);
 router.delete('/service/:id', serviceController.deleteService);
