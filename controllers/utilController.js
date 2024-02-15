30d54f2a308247f8582dcb6b551ede547a3c78f0
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-02-15 16:12:41 +0300

    Update utilController.js

--- controllers/utilController.js
+++ controllers/utilController.js
@@ -5,4 +5,24 @@ function subtractDate(date, hours) {
     return newDate;
 };
 
+function getAllMonths() {
+    const monthsName = [
+        "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+        "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
+    ];
+    const abbreviations = [
+        "Jan", "Fév", "Mar", "Avr", "Mai", "Jui",
+        "Juil", "Août", "Sept", "Oct", "Nov", "Déc"
+    ];
+    const months = [];
+    for(let i = 0; i < monthsName.length; i++){
+        const monthNumber = i + 1;
+        const monthName = monthsName[i];
+        const abbreviation = abbreviations[i];
+        months.push({monthNumber: monthNumber, monthName: monthName, abbreviation: abbreviation});
+    }
+    return months;
+}
+
 exports.subtractDate = subtractDate;
+exports.getAllMonths = getAllMonths;
