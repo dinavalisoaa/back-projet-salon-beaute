d061b6f03c2c36b3747ae16c7084fcc33fbcb74c
Nancia Rajerison <rajerisonnancia@gmail.com>
2024-02-20 11:11:57 +0300

    Commission for the day route

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -5,6 +5,7 @@ const employeeController = require('../controllers/employeeController');
 router.get('/employees', employeeController.getAllEmployee);
 router.get('/employee/:id', employeeController.getEmployee);
 router.get('/employees/active', employeeController.getActiveEmployee);
+router.get('/employee/:employeeId/commission', employeeController.commissionForTheDay);
 router.post('/employee', employeeController.registration);
 router.put('/employee/:id', employeeController.updateEmployee);
 router.put('/employee/:id/activate', employeeController.activateAccount);
