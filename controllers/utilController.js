ac357f9c4142da035b449dd8e2060a719b0cd26c
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-27 10:45:55 +0300

    Update utilController.js

--- controllers/utilController.js
+++ controllers/utilController.js
@@ -23,6 +23,23 @@ function getAllMonths() {
     }
     return months;
 }
+function subtractDatePart(date, hours) {
+    const newDate = new Date(date);
+    newDate.setHours(newDate.getHours() + hours);
+    // const datePart = {
+    //     day: new Date(newDate).getDate(),
+    //     month: new Date(newDate).getMonth() + 1,
+    //     hour: new Date(newDate).getHours(),
+    //     minute: new Date(newDate).getMinutes(),
+    // };
+    return new Date(new Date(newDate).getFullYear(),new Date(newDate).getMonth(),new Date(newDate).getDate(),new Date(newDate).getHours(),new Date(newDate).getMinutes());
+  }
+function getExpiration(){
+    const now=new Date(subtractDatePart(new Date(),3));
+    now.setHours(now.getHours()+1);
+    return now;
+}
 
 exports.subtractDate = subtractDate;
 exports.getAllMonths = getAllMonths;
+exports.getExpiration = getExpiration;
