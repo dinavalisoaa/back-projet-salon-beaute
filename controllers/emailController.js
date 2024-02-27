24854c9ff0012e1dfc643e3db8fc89835bb22efd
dinavalisoaa <randriamifidydina@gmail.com>
2024-02-27 10:45:59 +0300

    Update emailController.js

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const schedule = require('node-schedule');
 const cron = require('node-cron');
-const handlebars = require('handlebars');
+// const handlebars = require('handlebars');
 const fs = require('fs');
 
 const transporter = nodemailer.createTransport({
@@ -35,28 +35,28 @@ const transporter = nodemailer.createTransport({
 // };
 
 exports.sendEmail = async (req, res) => {
-    const { shipper, recipient, subject, customer, datetime } = req.body;
+    // const { shipper, recipient, subject, customer, datetime } = req.body;
 
-    const template = handlebars.compile(fs.readFileSync('templates/appointmentReminder.handlebars', 'utf8'));
+    // const template = handlebars.compile(fs.readFileSync('templates/appointmentReminder.handlebars', 'utf8'));
 
-    const html = template({ customer, datetime });
+    // const html = template({ customer, datetime });
 
-    const mailOptions = {
-        from: shipper + ' <' + process.env.USER + '>',
-        to: recipient,
-        subject: subject,
-        html: html
-    };
+    // const mailOptions = {
+    //     from: shipper + ' <' + process.env.USER + '>',
+    //     to: recipient,
+    //     subject: subject,
+    //     html: html
+    // };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error(error);
-            res.status(500).send('Erreur lors de l\'envoi de l\'e-mail');
-        } else {
-            console.log('E-mail envoyé: ' + info.response);
-            res.status(200).send('E-mail envoyé avec succès');
-        }
-    });
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //         console.error(error);
+    //         res.status(500).send('Erreur lors de l\'envoi de l\'e-mail');
+    //     } else {
+    //         console.log('E-mail envoyé: ' + info.response);
+    //         res.status(200).send('E-mail envoyé avec succès');
+    //     }
+    // });
 };
 
 exports.sendScheduledEmail = async (req, res) => {
